4be8eaae74d16b48413af1e017dd1245a17d0c16
maxfox97 <max.lisicin@gmail.com>
2022-03-29 12:27:20 +0300

    first commit

---
+++ src/componets/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+   return (
+      <header className="header">
+         <img src="https://uxwing.com/wp-content/themes/uxwing/download/36-arts-graphic-shapes/mobile-ui-ux.png"></img>
+      </header>
+   );
+};
+
+export default Header;
---
+++ src/componets/Profile.jsx
@@ -0,0 +1,29 @@
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://coreteka.com/wp-content/uploads/2020/08/difference-between-ux-ui-ux-ui-coreteka.png"></img>
+      </div>
+      <div>
+        Ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
